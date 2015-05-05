082c3a0ff84b8951f320255921da98d8acf2ff1a
shawnrushefsky <shawn@psymphonic.com>
2015-05-05 17:39:43 -0500

    Setting up the Git.
    Preparing a Python package to generate Psynth graphs.

---
+++ Setup.py
@@ -0,0 +1,11 @@
+__author__ = 'psymac0'
+import os
+from setuptools import setup
+
+setup(
+    name="psynthpy",
+    version="0.1.0",
+    description="An open-source Python library for generating graphs in Psymphonic Psynth."
+    url="http://github.com/shawnrushefsky/PsynthPy"
+
+)
