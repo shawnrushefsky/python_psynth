8db9df7cd325d1c05a7b4a75b3dc37bcb296ca9a
shawnrushefsky <shawn@psymphonic.com>
2015-05-09 20:43:15 -0500

    A better test.py

--- test.py
+++ test.py
@@ -1,36 +1,50 @@
 __author__ = 'psymac0'
-import psynth
+from psynth import *
 from random import randint
 
 
-
-g = psynth.create_graph(
+# Create a Graph
+g = create_graph(
     name='api test',
-    url='https://10.0.1.30/',
-    username='shawn@psymphonic.com',
-    key='9a6b250e-f9b3-4caf-81ae-14a062e15674'
+    url='http://psynth.psymphonic.com/',
+    username='user@company.com',
+    key='yourapikey'
 )
-for i in range(1,10):
-    n = psynth.Node(name='Node '+str(i))
+
+# Create 10 Nodes
+for i in range(0,10):
+    n = Node(name='Node '+str(i))
     g.add_node(n)
+
+# Grab a list of all the Nodes.
 ns = g.node_list()
-lt = psynth.LinkType()
+
+# Create a default LinkType
+lt = LinkType()
 g.add_link_type(lt)
-for i in range(1,30):
+
+# Create 30 random Links.
+for i in range(0,30):
     origin = randint(0, len(ns)-1)
     terminus = randint(0, len(ns)-1)
     while terminus == origin:
         terminus = randint(0, len(ns)-1)
-    link = psynth.Link(ns[origin].uid, ns[terminus].uid, lt.name, value=randint(1, lt.max))
+    link = Link(ns[origin].uid, ns[terminus].uid, lt.name, value=randint(1, lt.max))
     g.add_link(link)
+
+# Put 3 Details on each Node.
 for node in ns:
     for i in range(0, 3):
-        d = psynth.Detail("http://psymphonic.com", type='link')
+        d = Detail("http://psymphonic.com", type='link')
         node.add_detail(d)
+
+# Put 3 Details on each Link
 for link in g.link_list():
     for i in range(0, 3):
-        d = psynth.Detail("http://psymphonic.com", type='link')
+        d = Detail("http://psymphonic.com", type='link')
         link.add_detail(d)
+
+# Calculate the layout of the Graph
 g.draw()
 
 
